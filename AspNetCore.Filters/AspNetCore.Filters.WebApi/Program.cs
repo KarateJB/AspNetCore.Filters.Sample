e307ed6fcd9d3766623159c67d85c0dba31ee691
KarateJB <ppippenkimo@gmail.com>
2019-07-07 12:09:21 +0800

    Init project n enable NLog

---
+++ AspNetCore.Filters/AspNetCore.Filters.WebApi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace AspNetCore.Filters.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+                env.ConfigureNLog($"NLog.config");
+            })
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.SetMinimumLevel(LogLevel.Trace);
+            })
+            .UseNLog();
+    }
+}
---
+++ AspNetCore.Filters/AspNetCore.Filters.WebApi/Utils/LogFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCore.Filters.WebApi.Utils
+{
+    public class LogFilter: Attribute, IActionFilter
+    {
+        private readonly ILogger<LogFilter> _logger = null;
+
+        public LogFilter(ILogger<LogFilter> logger)
+        {
+            this._logger = logger;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
